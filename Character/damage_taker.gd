6fc901c55083dc89a7e6c0fb46bc46d24a15c70d
Quan <quandangv@gmail.com>
2019-11-25 15:12:45 +0700

    refining status animation

--- Character/damage_taker.gd
+++ Character/damage_taker.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 export var side = "zombie"
-export var pulse_effect_priorities = {"pulse-hurt": 10, "pulse-energy": 9, "pulse-temperature": 8, "pulse-hunger": 7, "pulse-energy-half":6, "pulse-temperature-half": 5, "pulse-hunger-half": 4, "pulsate-life": 3}
+export var critical_effect = ["pulse-hurt", "pulse-energy"]
+export var pulse_effect_priorities = {"pulse-temperature": 8, "pulse-hunger": 7, "pulse-energy-half":6, "pulse-temperature-half": 5, "pulse-hunger-half": 4, "pulsate-life": 3}
 var pulse_effects = PoolStringArray([])
 
 func _ready():
@@ -9,11 +10,14 @@ func _ready():
 
 func on_hit(source, tool_class, hit_strength):
 	get_parent().properties["life"].add_hit(-hit_strength)
-	$glow/anim.play("pulse-hurt")
 	ensure_play("pulse-hurt")
 
 func ensure_play(name):
-	pulse_effects.push_back(name)
+	if critical_effect.find(name) != -1:
+		$glow/anim.stop(true)
+		$glow/anim.play(name)
+	else:
+		pulse_effects.push_back(name)
 
 func status_process(delta):
 	var chosen_effect
@@ -23,6 +27,6 @@ func status_process(delta):
 		if priority > chosen_priority:
 			chosen_effect = effect
 			chosen_priority = priority
-	if chosen_effect != null and chosen_effect != "pulse-hurt":
+	if chosen_effect != null and critical_effect.find($glow/anim.current_animation) == -1:
 		$glow/anim.play(chosen_effect)
 	pulse_effects.resize(0)
